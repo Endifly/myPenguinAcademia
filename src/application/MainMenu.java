db0f2e4e60f1bbd321750c8145c549ba83b52e6c
Tanakorn Tampanya <ducth_miw@hotmail.com>
2018-12-06 22:36:57 +0700

    update PREFWIDTH for reference , set MainMenu to Pos.center

--- src/application/MainMenu.java
+++ src/application/MainMenu.java
@@ -1,17 +1,23 @@
 package application;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import util.reference;
 
 public class MainMenu extends VBox{
 	private Button start;
 	private Button exit;
 	
 	public MainMenu() {
-		super();
+		super(10);
+		this.setAlignment(Pos.CENTER);
 		start = new Button("Play");
 		exit = new Button("Exit");
 		
+		start.setAlignment(Pos.CENTER);
+		start.setPrefWidth(util.reference.PREFWIDTH);
+		exit.setPrefWidth(util.reference.PREFWIDTH);
 		this.getChildren().addAll(start,exit);
 	}
 
--- src/util/reference.java
+++ src/util/reference.java
@@ -3,4 +3,5 @@ package util;
 public class reference {
 	public static int WIDTH = 1280;
 	public static int HIGH = 720;
+	public static int PREFWIDTH = 200;
 }
