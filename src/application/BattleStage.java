ae2ed24b6fc1ae33bf78203956c9de04c1c247a9
Tanakorn Tampanya <ducth_miw@hotmail.com>
2018-12-06 22:54:38 +0700

    test max display

--- src/application/BattleStage.java
+++ src/application/BattleStage.java
@@ -1,5 +1,8 @@
 package application;
 
+import util.reference;
+
+import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
@@ -8,8 +11,6 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-
-
 public class BattleStage extends VBox{
 	private ProgressBar HP;
 	private Label score;
@@ -17,19 +18,28 @@ public class BattleStage extends VBox{
 	private Canvas penguin1;
 	
 	public BattleStage() {
-		super();
+		super(10);
 		HBox status = new HBox();
+		status.setAlignment(Pos.CENTER);
 		
 		HP = new ProgressBar(1);
-		HP.setPrefWidth(200);
+		HP.setPrefWidth(util.reference.PREFWIDTH);
 		score = new Label("0");
-		
+		score.setPrefWidth(util.reference.PREFWIDTH);
+		HP.setPrefWidth(util.reference.PREFWIDTH);
 		
 		player1 = new Penguin();
-		penguin1 = new Canvas(600, 600);
+		penguin1 = new Canvas(util.reference.WIDTH, util.reference.HIGH-60);
 		GraphicsContext penguin1GC = penguin1.getGraphicsContext2D();
 		penguin1GC.drawImage(this.LoadImage("untitled.png"), 0, 0);
 		penguin1GC.drawImage(this.LoadImage("untitled.png"), 152, 118);
+		penguin1GC.drawImage(this.LoadImage("untitled.png"), 2*152, 2*118);
+		penguin1GC.drawImage(this.LoadImage("untitled.png"), 3*152, 3*118);
+		penguin1GC.drawImage(this.LoadImage("untitled.png"), 4*152, 4*118);
+		penguin1GC.drawImage(this.LoadImage("untitled.png"), 5*152, 5*118);
+		penguin1GC.drawImage(this.LoadImage("untitled.png"), 6*152, 4*118);
+		penguin1GC.drawImage(this.LoadImage("untitled.png"), 7*152, 3*118);
+		penguin1GC.drawImage(this.LoadImage("untitled.png"), 8*152, 2*118);
 		
 		status.getChildren().addAll(score,HP);
 		this.getChildren().addAll(status,penguin1);
--- src/util/reference.java
+++ src/util/reference.java
@@ -1,6 +1,7 @@
 package util;
 
 public class reference {
+	//for keep all constant for program.
 	public static int WIDTH = 1280;
 	public static int HIGH = 720;
 	public static int PREFWIDTH = 200;
