d2fdd2cc4568e9da7316a4e082a10ef1498fd929
Tanakorn Tampanya <ducth_miw@hotmail.com>
2018-12-06 22:12:31 +0700

    draw image

--- src/application/BattleStage.java
+++ src/application/BattleStage.java
@@ -1,19 +1,25 @@
 package application;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
+
+
 public class BattleStage extends VBox{
 	private ProgressBar HP;
 	private Label score;
 	private Penguin player1;
+	private Canvas penguin1;
 	
 	public BattleStage() {
-		super(100);
+		super();
 		HBox status = new HBox();
 		
 		HP = new ProgressBar(1);
@@ -22,9 +28,15 @@ public class BattleStage extends VBox{
 		
 		
 		player1 = new Penguin();
+		penguin1 = new Canvas(600, 300);
+		GraphicsContext penguin1GC = penguin1.getGraphicsContext2D();
+		penguin1GC.drawImage(this.LoadImage("dinosaur.jpg"), 0, 0);
 		
 		status.getChildren().addAll(score,HP);
-		this.getChildren().addAll(status);
+		this.getChildren().addAll(status,penguin1);
 		
+	}
+	private Image LoadImage(String imagePath) {
+		return new Image(ClassLoader.getSystemResource(imagePath).toString());
 	}
 }
